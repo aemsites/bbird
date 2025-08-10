df60106b0dea0bec835d7374b154cced9fc92c5a
Mohammed junaid Iqbal khalidi <moiqbal@Mohammeds-MacBook-Pro.local>
2025-08-10 14:19:28 -0700

    done

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -8,11 +8,22 @@ export default function decorate(block) {
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+      else if (div.querySelector('a') || div.querySelector('button')) div.className = 'cards-card-button';
       else div.className = 'cards-card-body';
     });
     ul.append(li);
   });
   ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+  
+  // Add button functionality
+  ul.querySelectorAll('.cards-card-button a, .cards-card-button button').forEach((button) => {
+    if (button.tagName === 'A') {
+      button.className = 'cards-button cards-button--link';
+    } else if (button.tagName === 'BUTTON') {
+      button.className = 'cards-button cards-button--button';
+    }
+  });
+  
   block.textContent = '';
   block.append(ul);
 }
